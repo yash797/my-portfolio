11e0d2a5f12c7627b76e1d190c77bcb98601d60c
Yash <yashpande2002@gmail.com>
2022-09-12 22:38:36 +0530

    squid game added

--- src/components/Squid.js
+++ src/components/Squid.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import squids from "../images/squids.png";
+import Loader from "../components/loader";
 
-function squid() {
+const Squid = () => {
+  const [loading, setLoading] = useState(false)
+ useEffect(()=>{setTimeout(() => {
+    setLoading(true)
+ }, 2000);})
   return (
+    <div>
+    {loading===false ?
+    <div className=' h-screen w-0'> <Loader/> </div>: 
     <div className='h-[80px] w-full'>
       <img src= {squids}/>
-    </div>
+    </div> }</div>
   )
 }
 
-export default squid
+export default Squid
